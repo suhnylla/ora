ba6d53ba1a0fd3320368e86923fad0e3452b88ce
darthpasta <liowj.nick@gmail.com>
2017-06-16 00:31:48 +0800

    Added seed avatar(limit 5)

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
 	def homepage
 		@offers = Offer.all.order("created_at DESC")#.page(params[:page])	
+		@users = User.all.order("created_at DESC").limit(5)
 	end
 end
