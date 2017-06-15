0108160cac50669b3882deb2b6ebe7d8407562e5
darthpasta <liowj.nick@gmail.com>
2017-06-15 23:15:44 +0800

    added db seed

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 gem 'omniauth-facebook'
+gem 'faker'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+	50.times do 
+		User.create(
+			:email => Faker::Internet.email,
+			:password => Faker::Name.name
+			)
+	end
+
+	50.times do
+		Offer.create(
+			:title => Faker::LordOfTheRings.character,
+			:offer_description => Faker::ChuckNorris.fact,
+			:offer_category => Faker::Pokemon.name,
+			:token => rand(1..100),
+			:user_id => rand(1..50)
+			)
+	end
+end
