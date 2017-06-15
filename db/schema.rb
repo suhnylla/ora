b1cdc2d78d4e28f40196a32ee29ed4a276e1922c
zoelim7 <zoelim7@gmail.com>
2017-06-15 17:55:25 +0800

    migration + test

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 20170615082348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.text "offer_description"
+    t.string "offer_category"
+    t.integer "token"
+    t.string "photo"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,4 +46,5 @@ ActiveRecord::Schema.define(version: 20170615082348) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offers", "users"
 end
