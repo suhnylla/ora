bc1f5c475fed2ea4c33a756d3843aea385cb050d
darthpasta <liowj.nick@gmail.com>
2017-06-16 01:34:22 +0800

    added limit to offer in index page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
 	def homepage
-		@offers = Offer.all.order("created_at DESC")#.page(params[:page])	
+		@offers = Offer.all.order("created_at DESC").limit(20)#.page(params[:page])	
 		@users = User.all.order("created_at DESC").limit(5)
 	end
 end
